c4f944a889696e195aa4c55f4ea97f3415f15425
root <root@ethical-post-1.localdomain>
2026-09-12 14:44:11 +0000

    disable quartz default theme and add a lock for auto-syncx

--- auto-sync.sh
+++ auto-sync.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+LOCKFILE="/var/run/quartz-build.lock"
+
+exec 9>"$LOCKFILE"
+
+if ! flock -n 9; then
+    echo "Quartz build is already running."
+    exit 0
+fi
+
 export NVM_DIR="/root/.nvm"
 source "$NVM_DIR/nvm.sh"
 
